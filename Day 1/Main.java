45326d04ac06c4bc86e7fe468bacee8aa0f4a25b
Nehhhaagb <cneha2404@gmail.com>
2025-06-10 11:21:52 +0530

    initial commit

---
+++ Day 1/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+      // boolean b1 = true ;
+      // boolean b2 = false ;
+      // int n = 16545;
+      // int p = 1649696;
+      // float pi= 3.14f;
+      // char c = 'A';
+      // String s = "Hello World";
+
+
+
+      // System.out.println("is java funny? " + b1  );
+      // System.out.println(n);
+      // System.out.println(pi);
+      // System.out.println(s + 2);
+      // System.out.println(n + p);
+
+      // int num1 = 24;
+      // int num2 =25;
+      // char first = 'A';
+      // System.out.println(num1 + num2);
+      // System.out.println("first" + num2);
+      // System.out.println(first + num2);
+
+    // change in data type 
+    String str = "123";
+    String str2 = "456";
+    
+    int a1 = Integer.parseInt(str);
+    int a2 = Integer.parseInt(str2 );
+
+    System.out.println(a2 + a1);
+    System.out.println(str+ a1);
+
+
+    }
+  }
+  
---
+++ Day 1/pars.java
@@ -0,0 +1,40 @@
+// Java Program to show the use of
+// Arithmetic Operators
+import java.io.*; 
+
+class pars
+{
+    public static void main (String[] args) 
+    {
+          
+        // // Arithmetic operators on integers
+        // int a = 10;
+        // int b = 3;
+      
+        // // Arithmetic operators on Strings
+        // String n1 = "15";
+        // String n2 = "25";
+
+        // // Convert Strings to integers
+        // int a1 = Integer.parseInt(n1);
+        // int b1 = Integer.parseInt(n2);
+           
+        // // System.out.println("a + b = " + (a + b));
+        // // System.out.println("a - b = " + (a - b));
+        // // System.out.println("a1 + b1 = " + (a1 + b1)); 
+        // // System.out.println("n1 + n2 = " + (n1 + n1)); 
+        // // System.out.println(a+ n1);
+        // // System.out.println("10"+ a );
+        
+        // // int ans = 50 - n1 ;
+        // //System.out.println(50 - n1); 
+        // System.out.println(50 - a1);
+
+          
+        // int a =5;
+        // int b = 10;
+        // System.out.println(a > b);
+
+        // System.out.println(0 || 1);
+    }
+}
